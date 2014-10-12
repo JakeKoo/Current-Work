e119bf797fd74dff8fe0f01e3d86a633c89fed32
Jake Kooiman <jake.kooiman@gmail.com>
2014-10-12 12:06:02 -0500

    Added pre-existing Workspace into folder

---
+++ workspace/FancyPantAdventure/src/mainSystem/Player.java
@@ -0,0 +1,42 @@
+package mainSystem;
+
+public class Player {
+	
+	//Chosen name at the start of game
+	String player_name;
+	//Altered name chosen at prompts due to events
+	String altered_player_name;
+	
+	//RPG style stat system
+	//Dodge; Quick weapon accuracy; # of attacks
+	int stat_dexterity;
+	
+	//Weapon/Armor limits; strong weapon accuracy; damage bonus
+	int stat_strength;
+	
+	//Multiplier for relation increase; discount; flirt
+	int stat_charisma;
+	
+	//Crit multiplier; identify things
+	int stat_intelligence;
+	
+	//MP determiner; better results from items
+	int stat_wisdom;
+	
+	//HP; Amount of energy per day
+	int stat_stamina;
+	
+	//Body Descriptors
+	
+	//Height will determine how tall the character is. At certain points
+	//it should affect strength based things
+	int player_height;
+	
+	//Chest size, flat chest to full bust
+	int player_chest_size;
+	
+	//Waist thickness
+	int player_waist_thickness;
+	
+	//
+}
